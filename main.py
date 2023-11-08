a6b76057e9b252be7b03ef339ea13c540f6360da
gbgithub1234 <142700922+gbgithub1234@users.noreply.github.com>
2023-11-07 18:26:12 -0800

    Update main.py
    copied code from github file, overriding local

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ import streamlit as st
 from streamlit_chat import message
 
 
-st.header("SFU AAE Chatbot 1.0 (beta)")
-# st.header("LangChain Udemy Course- Documentation Helper Bot")
-
-
 #------------------------------------------
-
 url = "https://drive.google.com/drive/u/0/folders/1gTD-OiqH5Bg3-ZqVuur9q8h-AGIzOlB7"
 url2 = "https://www.pinecone.io/"
 
 multiline_str1 = """
+
+- ***this page is currently being updated. I apologize for the inconvenience*** \n
+
 - created by Glen Brauer, Business Analyst in AAE (glenb@sfu.ca) \n
 
 - PROBLEM: document-based information is located in many places taking time to find\n
@@ -42,6 +40,8 @@ with st.expander("Show/hide details"):
     # st.write("- documents which have been ingested are located [here](%s)" % url)
 
 #------------------------------------------
+st.header("SFU Document Chatbot 1.1 (beta)")
+
 
 if "user_prompt_history" not in st.session_state:
     st.session_state["user_prompt_history"] = []
@@ -69,40 +69,43 @@ def create_sources_string(source_urls: Set[str]) -> str:
 
 
 
-with st.form(key='myform', clear_on_submit=True):
-    prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
-    submit_button = st.form_submit_button("Submit")
+#with st.form(key='myform', clear_on_submit=True):
+    #prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
+    #submit_button = st.form_submit_button("Submit")
+    
     # message("message 1", is_user=True)
     # message("message 2")
 
 
-if submit_button:
-    with st.spinner("Generating response.."):
-        generated_response = run_llm(
-            query=prompt, chat_history=st.session_state["chat_history"]
-        )
+# if submit_button:
+#     with st.spinner("Generating response.."):
+#         generated_response = run_llm(
+#             query=prompt, chat_history=st.session_state["chat_history"]
+#         )
+# 
+# 
+# 
+# 
+#         sources = set(
+#             [doc.metadata["source"] for doc in generated_response["source_documents"]]
+#         )
+# 
+#         formatted_response = (
+#             f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
+#         )
+# 
+# 
+#         #-----------------
+#         message(prompt, is_user=True)
+#         message(formatted_response)
+#         #-----------------
+# 
+# 
+#         st.session_state["user_prompt_history"].append(prompt)
+#         st.session_state["chat_answers_history"].append(formatted_response)
+#         st.session_state["chat_history"].append((prompt, generated_response["answer"]))
+# 
 
-
-
-
-        sources = set(
-            [doc.metadata["source"] for doc in generated_response["source_documents"]]
-        )
-
-        formatted_response = (
-            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
-        )
-
-
-        #-----------------
-        message(prompt, is_user=True)
-        message(formatted_response)
-        #-----------------
-
-
-        st.session_state["user_prompt_history"].append(prompt)
-        st.session_state["chat_answers_history"].append(formatted_response)
-        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
 
         # clear the inputbox
         # st.session_state["Prompt"] = ""
@@ -117,13 +120,3 @@ if submit_button:
 #     ):
 #         message(user_query, is_user=True)
 #         message(generated_response)
-
-
-
-
-#------------------------------------------
-
-
-
-# if prompt:
-
